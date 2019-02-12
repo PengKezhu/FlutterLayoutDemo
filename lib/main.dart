1599bfbfecce59dba5f9f934f3400cde6642b9c7
kezhu <pengkezhu@7mtt.cn>
2019-02-12 18:29:47 +0800

    learn some widgets

--- lib/main.dart
+++ lib/main.dart
@@ -82,8 +82,251 @@ class MyApp extends StatelessWidget {
 Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.。。
         ''',
         softWrap: true,
+        style: TextStyle(color: Colors.red),
       ),
     );
+
+    var descTextStyle = new TextStyle(
+        color: Colors.red, fontSize: 20, fontStyle: FontStyle.italic);
+
+    Widget leftColum = new ListView(
+      children: [
+        new Image.asset(
+          'images/lake.png',
+          width: 600.0,
+          height: 240.0,
+          fit: BoxFit.cover,
+        ),
+        titleSection,
+        buttonSection,
+        textSection,
+        new Row(
+          children: <Widget>[
+            new Center(
+              child: new Text(
+                'data',
+                style: TextStyle(
+                    color: Colors.yellow,
+                    fontSize: 30,
+                    textBaseline: TextBaseline.alphabetic),
+              ),
+            ),
+            new Image.asset('images/lake.png', fit: BoxFit.cover),
+            new Icon(Icons.network_wifi, color: Colors.red)
+          ],
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        ),
+        new Row(
+          children: <Widget>[
+            new Expanded(
+              child: new Image.asset(
+                'images/lake.png',
+                fit: BoxFit.cover,
+              ),
+              flex: 1,
+            ),
+            new Expanded(
+              child: new Image.asset(
+                'images/lake.png',
+                fit: BoxFit.cover,
+              ),
+              flex: 1,
+            ),
+            new Expanded(
+              child: new Image.asset(
+                'images/lake.png',
+                fit: BoxFit.cover,
+              ),
+              flex: 1,
+            ),
+          ],
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.min,
+        ),
+        new Image.asset(
+          'images/lake.png',
+          width: 5000,
+          height: 200,
+          fit: BoxFit.fill,
+        ),
+        new Container(
+          padding: new EdgeInsets.all(20),
+          child: new Row(
+            children: <Widget>[
+              new Row(
+                children: <Widget>[
+                  Icon(Icons.star),
+                  Icon(Icons.star),
+                  Icon(Icons.star),
+                  Icon(Icons.star),
+                  Icon(Icons.star),
+                ],
+              ),
+              new Text(
+                '170评价',
+                textAlign: TextAlign.center,
+              ),
+            ],
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          ),
+        ),
+        new DefaultTextStyle(
+          style: descTextStyle,
+          child: new Container(
+            padding: new EdgeInsets.all(20.0),
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new Column(
+                  children: <Widget>[
+                    new Icon(Icons.kitchen),
+                    new Text("PREP:"),
+                    new Text("25 min")
+                  ],
+                ),
+                new Column(
+                  children: <Widget>[
+                    new Icon(Icons.timer),
+                    new Text("COOK:"),
+                    new Text("1 hr")
+                  ],
+                ),
+                new Column(
+                  children: <Widget>[
+                    new Icon(Icons.restaurant),
+                    new Text("FEEDS:"),
+                    new Text("4-6")
+                  ],
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+
+    Widget card = new Card(
+        child: new Column(
+      children: <Widget>[
+        new Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            new Container(
+              width: 100,
+              color: Colors.green,
+              child: new Text("data"),
+            ),
+            new Image.asset(
+              'images/lake.png',
+              fit: BoxFit.cover,
+            )
+          ],
+        ),
+        new Container(
+            color: Colors.black26,
+            child: new Column(
+              children: <Widget>[
+                new Row(
+                  children: <Widget>[
+                    new Expanded(
+                      child: new Container(
+                        decoration: new BoxDecoration(
+                            border: new Border.all(
+                                width: 10.0, color: Colors.black38),
+                            borderRadius: const BorderRadius.all(
+                                const Radius.circular(8.0))),
+                        child: new Image.asset('images/lake.png'),
+                        margin: const EdgeInsets.all(4.0),
+                      ),
+                    ),
+                    new Expanded(
+                      child: new Container(
+                        decoration: new BoxDecoration(
+                            border: new Border.all(
+                                width: 10.0, color: Colors.black38),
+                            borderRadius: const BorderRadius.all(
+                                const Radius.circular(8.0))),
+                        child: new Image.asset('images/lake.png'),
+                        margin: const EdgeInsets.all(4.0),
+                      ),
+                    ),
+                  ],
+                ),
+                new Row(
+                  children: <Widget>[
+                    new Expanded(
+                      child: new Container(
+                        decoration: new BoxDecoration(
+                            border: new Border.all(
+                                width: 10.0, color: Colors.black38),
+                            borderRadius: const BorderRadius.all(
+                                const Radius.circular(8.0))),
+                        child: new Image.asset('images/lake.png'),
+                        margin: const EdgeInsets.all(4.0),
+                      ),
+                    ),
+                    new Expanded(
+                      child: new Container(
+                        decoration: new BoxDecoration(
+                            border: new Border.all(
+                                width: 10.0, color: Colors.black38),
+                            borderRadius: const BorderRadius.all(
+                                const Radius.circular(8.0))),
+                        child: new Image.asset('images/lake.png'),
+                        margin: const EdgeInsets.all(4.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )),
+        // new Container(
+        //   height: 500,
+        //   width: 300,
+        //   child: new GridView.extent(
+        //     maxCrossAxisExtent: 150.0,
+        //     padding: new EdgeInsets.all(10.0),
+        //     mainAxisSpacing: 5.0,
+        //     crossAxisSpacing: 5.0,
+        //     children: <Widget>[
+        //       // new Image.asset('images/lake.png'),
+        //       // new Image.asset('images/lake.png'),
+        //       // new Image.asset('images/lake.png'),
+        //       // new Image.asset('images/lake.png'),
+        //     ],
+        //   ),
+        // )
+      ],
+    ));
+
+    List<Widget> list = <Widget>[
+      new ListTile(
+        title: new Text('1'),
+        leading: new Icon(
+          Icons.school,
+          color: Colors.green,
+        ),
+        subtitle: new Text('description'),
+      ),
+      new ListTile(
+        title: new Text('2'),
+        leading: new Icon(
+          Icons.school,
+          color: Colors.blue,
+        ),
+        subtitle: new Text('description'),
+      ),
+      new ListTile(
+        title: new Text('3'),
+        leading: new Icon(Icons.school),
+        subtitle: new Text('description'),
+      ),
+      new ListTile(
+          title: new Text('4'),
+          leading: new Icon(Icons.school),
+          subtitle: new Text('description'),
+          onTap: () {})
+    ];
 
     return new MaterialApp(
       title: 'Flutter Demo',
@@ -91,108 +334,23 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        appBar: new AppBar(
-          title: Text('Flutter layout Demo'),
-        ),
-        body: new ListView(
-          children: [
-            new Image.asset(
-              'images/lake.png',
-              width: 600.0,
-              height: 240.0,
-              fit: BoxFit.cover,
-            ),
-            titleSection,
-            buttonSection,
-            textSection
-          ],
-        ),
-      ),
+          appBar: new AppBar(
+            title: Text('Flutter layout Demo'),
+          ),
+          body: new Container(
+              //     child: new ListView(
+              //   children: list,
+              // )
+              child: new Stack(
+            alignment: Alignment(0.6, 0.6),
+            children: <Widget>[
+              new CircleAvatar(
+                backgroundImage: new AssetImage('images/lake.png'),
+                radius: 160.0,
+              ),
+              new Text('data')
+            ],
+          ))),
     );
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key key, this.title}) : super(key: key);
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 10;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Invoke "debug painting" (press "p" in the console, choose the
-//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//           // to see the wireframe for each widget.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.display1,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
